0a35c063e7e3ebe64751ca97188d9e92e8b380d4
Gepser Hoil <gepser.hoil@xoom.com>
2017-06-23 12:36:27 -0600

    [master] Cleaning a little bit the homebrewtools

--- manifests/brewtools.pp
+++ manifests/brewtools.pp
@@ -1,11 +1,18 @@
-class gettingstarted::brewtools {
+class gettingstarted::brewtools () {
 
-    pkglist = [
+    class { 'homebrew':
+        user  => 'ghoil', #we need a parameter for this
+        group => 'admin',
+    }
+
+    $pkglist = [
         'bash-completion',
         'wget',
         'ctags',
         'git',
         'git-flow',
+        'openssl',
+        'libyaml',
         'reattach-to-user-namespace',
         'the_silver_searcher',
         'tmux',
@@ -20,28 +27,24 @@ class gettingstarted::brewtools {
         'google-chrome',
         ]
 
-    package { $pkglist:
-        ensure   => present,
-        provider => brew,
-    }
+    $taplist = [
+        'homebrew/core',
+        'buo/cask-upgrade',
+        'thoughtbot/formulae',
+        'caskroom/versions',
+    ]
 
-    package { 'openssl':
-        ensure   => present,
-        provider => brew,
-    }
-
-    package { 'buo/cask-upgrade':
-        ensure   => present,
-        provider => tap,
-    }
-
-    package { 'libyaml':
-        ensure   => present,
-        provider => brew,
-        require  => Package['openssl'],
-    }
-
-    casklist = [
+    $casklist = [
+        'qlcolorcode',
+        'qlstephen',
+        'qlmarkdown',
+        'quicklook-json',
+        'qlprettypatch',
+        'quicklook-csv',
+        'betterzipql',
+        'qlimagesize',
+        'webpquicklook',
+        'suspicious-package',
         'atom',
         'intellij-idea',
         'araxis-merge',
@@ -49,10 +52,25 @@ class gettingstarted::brewtools {
         'vagrant',
         'vagrant-manager',
         'docker',
+        'hipchat',
+        'java',
+        'java6',
     ]
+
+    package { $taplist:
+        ensure   => present,
+        provider => tap,
+    }
+
+    package { $pkglist:
+        ensure   => present,
+        provider => brew,
+        require  => Package[$taplist],
+    }
 
     package { $casklist:
         ensure   => present,
         provider => brewcask,
+        require  => Package[$pkglist],
     }
 }
