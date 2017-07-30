a3c6b07a616862389b3683af6d065217297bb35f
Gepser Hoil <geharold@gmail.com>
2017-07-30 14:30:31 -0600

    Keeping it minimalist. We will extend it with puppet modules if we want

--- manifests/brewtools.pp
+++ manifests/brewtools.pp
@@ -1,31 +1,19 @@
-class gettingstarted::brewtools {
+class osx::brewtools {
 
     pkglist = [
-        'bash-completion',
+        'openssl',
         'wget',
-        'ctags',
         'git',
         'git-flow',
-        'reattach-to-user-namespace',
-        'the_silver_searcher',
-        'tmux',
         'tree',
-        'pidof',
         'jq',
         'ctop',
         'go',
         'node',
-        'vim',
-        'firefox',
         'google-chrome',
         ]
 
     package { $pkglist:
-        ensure   => present,
-        provider => brew,
-    }
-
-    package { 'openssl':
         ensure   => present,
         provider => brew,
     }
@@ -35,16 +23,7 @@ class gettingstarted::brewtools {
         provider => tap,
     }
 
-    package { 'libyaml':
-        ensure   => present,
-        provider => brew,
-        require  => Package['openssl'],
-    }
-
     casklist = [
-        'atom',
-        'intellij-idea',
-        'araxis-merge',
         'virtualbox',
         'vagrant',
         'vagrant-manager',
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,3 @@
-class gettingstarted {
-    include gettingstarted::brewtools
+class osx {
+    include osx::brewtools
 }
